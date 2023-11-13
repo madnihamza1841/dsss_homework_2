b7a24d8eaf137f13fdb76b0edd5249b5d4cfc970
Muhammad Hamza Madni <hamzamadni@Muhammads-Air.fritz.box>
2023-11-13 22:12:56 +0100

    Code refactored and bugs resolved

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,38 +1,72 @@
 import random
 
 
-def function_A(min, max):
+def generate_random_number(min, max):
     """
-    Random integer.
+    The function generates a random integer between min and max.
+
+    Args:
+        min: The value of the lower bound for random number
+        max: The value of the upper bound for the random number
+
+    Returns:
+        A random number is returned between min and max
     """
     return random.randint(min, max)
 
 
-def function_B():
+def choose_random_operator():
+    """
+    The function chooses for a randomly selected operator from +,- and *.
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def solve_problems(n1, n2, operator):
+    """
+    The function uses two variables and an operator to create equations between them.
+
+    Args:
+        n1: The first number
+        n2: The second number
+        operator: The operator
+
+    Returns:
+        The function returns the equaiton and the solution to the equation
+    """
+    equation = f"{n1} {operator} {n2}"
+    solution = None
+
+    if operator == '+':
+        solution = n1 + n2
+    elif operator == '-':
+        solution = n1 - n2
+    elif operator == '*':
+        solution = n1 * n2
+    else:
+        return 'Operator has not been handled'
+    return equation, solution
 
 def math_quiz():
     s = 0
-    t_q = 3.14159265359
+    t_q = 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        n1 = generate_random_number(1, 10)
+        n2 = generate_random_number(1, 5)
+        operator = choose_random_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = solve_problems(n1, n2, operator)
         print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        
+        try:
+            useranswer = int(input("Your answer: "))
+        except ValueError:
+            print("You did not enter a valid integer.")
+            useranswer = -100000 #Assigned a value beyond our answers
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
